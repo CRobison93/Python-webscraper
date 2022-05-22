c60ef7328410b351f172538a040bd870d9fab5f7
CRobison93 <103977677+CRobison93@users.noreply.github.com>
2022-05-22 16:15:32 -0500

    new commit

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,1 @@
-# Selenium web scraper #
-import selenium
-from selenium import webdriver
-
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-
-driver.get("https://techwithtime.net")
+import 
